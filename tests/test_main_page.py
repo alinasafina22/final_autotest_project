b8ceb8a92241049bda7df5bbb77787c4dea20792
alinasafina22 <alinasafina22@gmail.com>
2023-04-24 16:07:52 +0500

    rename file

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -2,10 +2,10 @@
 1. Добавьте в файл conftest.py обработчик, который считывает из командной строки параметр language.
 2. Реализуйте в файле conftest.py логику запуска браузера с указанным языком пользователя. Браузер должен объявляться
 в фикстуре browser и передаваться в тест как параметр.
-3. В файл test_items.py напишите тест, который проверяет, что страница товара на сайте содержит кнопку добавления в корзину
+3. В файл test_main_page.py напишите тест, который проверяет, что страница товара на сайте содержит кнопку добавления в корзину
 Например, можно проверять товар, доступный по http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/.
 4. Тест должен запускаться с параметром language следующей командой:
-pytest --language=es test_items.py
+pytest --language=es test_main_page.py
 '''
 from selenium.webdriver.common.by import By
 from time import sleep
